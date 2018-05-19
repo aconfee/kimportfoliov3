871cc7f5644504e9cc4137192252689d7b5c707f
aconfee <adamestela@gmail.com>
2018-05-19 16:31:03 -0500

    scrolltop bug

--- src/pages/projects/projects.component.js
+++ src/pages/projects/projects.component.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../home/home.component.css';
 import ResponsiveImage from '../../atoms/responsiveImage/responsiveImage.component.js';
 import { Link } from 'react-router-dom';
 
-function Projects() {
-  return (
-    <div>
-      <Link to="/carcajou">
-        <ResponsiveImage imageUrl="./images/projects/carcajou/project_cover_carcajou.png" />
-      </Link>
-      <Link to="/alice">
-        <ResponsiveImage imageUrl="./images/projects/alice/project_cover_alice.png" />
-      </Link>
-      <Link to="/fishbowls">
-        <ResponsiveImage imageUrl="./images/projects/fishbowls/project_cover_fishbowls.png" />
-      </Link>
-      <Link to="/jesse">
-        <ResponsiveImage imageUrl="./images/projects/jesse/project_cover_jesse.png" />
-      </Link>
-      <Link to="/lilly">
-        <ResponsiveImage imageUrl="./images/projects/lilly/project_cover_lilly.png" />
-      </Link>
-    </div>
-  );
+class Projects extends Component {
+
+  onComponentWillMount = () => {  
+    window.scrollTo(0, 0);
+  };
+
+  render() {
+    return (
+      <div>
+        <Link to="/carcajou">
+          <ResponsiveImage imageUrl="./images/projects/carcajou/project_cover_carcajou.png" />
+        </Link>
+        <Link to="/alice">
+          <ResponsiveImage imageUrl="./images/projects/alice/project_cover_alice.png" />
+        </Link>
+        <Link to="/fishbowls">
+          <ResponsiveImage imageUrl="./images/projects/fishbowls/project_cover_fishbowls.png" />
+        </Link>
+        <Link to="/jesse">
+          <ResponsiveImage imageUrl="./images/projects/jesse/project_cover_jesse.png" />
+        </Link>
+        <Link to="/lilly">
+          <ResponsiveImage imageUrl="./images/projects/lilly/project_cover_lilly.png" />
+        </Link>
+      </div>
+    );
+  };
 };
 
 export default Projects;
