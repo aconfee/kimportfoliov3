8a5b559de0b6cf7c0808a7db53dfe44b33635bcd
aconfee <adamestela@gmail.com>
2018-05-19 19:46:29 -0500

    new route and remove api endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,5 @@ var router = express.Router();
 var contact = require('../controllers/contact.controller.js');
 
 router.post("/contact", contact.sendMessage);
-router.get("/feed", contact.instagramFeed);
 
 module.exports = router;
