6dfc9a0a3bee530cfc17350390ebcb25755d287d
Adam Estela <adamestela@Adams-MacBook-Air.local>
2018-08-04 14:01:57 -0500

    Post LA trip tweaks.

--- src/pages/home/home.component.js
+++ src/pages/home/home.component.js
@@ -3,33 +3,29 @@ import './home.component.css';
 import Gallery from '../../molecules/gallery/gallery.component';
 import ContactForm from '../../organisms/forms/contactForm.component.js';
 
-const GRINCH_PATH = "./images/projects/grinch/";
-const CARCAJOU_PATH = "./images/projects/carcajou/";
-const FISHBOWLS_PATH = "./images/projects/fishbowls/";
+const GALLERY_PATH = "./images/galleries/portfolio/";
 
 function Home() {
   return (
     <div>
       <Gallery imageRowUrls={[
-        [GRINCH_PATH + "Grinch_00.png"],
-        [GRINCH_PATH + "Grinch_01.png"],
-        [GRINCH_PATH + "Grinch_02.png"],
-        [GRINCH_PATH + "Grinch_03.png"],
-        [GRINCH_PATH + "Grinch_05.png"],
-        [GRINCH_PATH + "Grinch_06.png"],
-        [GRINCH_PATH + "Grinch_07.png"],
-        [CARCAJOU_PATH + "vincent_testimony.jpg"],
-        [CARCAJOU_PATH + "Carcajou_00.png"],
-        [CARCAJOU_PATH + "carcajou_01.png"],
-        [CARCAJOU_PATH + "carcajou_02.png"],
-        [CARCAJOU_PATH + "carcajou_03.png"],
-        [CARCAJOU_PATH + "carcajou_04.png"],
-        [CARCAJOU_PATH + "carcajou_05.png"],
-        [CARCAJOU_PATH + "carcajou_06.png"],
-        [CARCAJOU_PATH + "carcajou_07.png"],
-        [CARCAJOU_PATH + "carcajou_08.png"],
-        [FISHBOWLS_PATH + "Fishbowls_02.png"],
-        [FISHBOWLS_PATH + "Fishbowls_01.png"]
+        [GALLERY_PATH + "000_grinch.png"],
+        [GALLERY_PATH + "010_grinch.png"],
+        [GALLERY_PATH + "020_grinch.png"],
+        [GALLERY_PATH + "030_grinch.png"],
+        [GALLERY_PATH + "040_grinch.png"],
+        [GALLERY_PATH + "050_grinch.png"],
+        [GALLERY_PATH + "060_grinch.png"],
+        [GALLERY_PATH + "070_carcajou.png"],
+        [GALLERY_PATH + "080_carcajou.png"],
+        [GALLERY_PATH + "090_carcajou.png"],
+        [GALLERY_PATH + "100_carcajou.png"],
+        [GALLERY_PATH + "110_carcajou.png"],
+        [GALLERY_PATH + "120_carcajou.png"],
+        [GALLERY_PATH + "130_carcajou.png"],
+        [GALLERY_PATH + "140_carcajou.png"],
+        [GALLERY_PATH + "150_fishbowls.png"],
+        [GALLERY_PATH + "160_fishbowls.png"]
       ]} />
       <br />
       <br />
