6cda247adfe91806251711a4eda5af874e5ff9af
Adam Estela <adamestela@Adams-MacBook-Air.local>
2018-11-14 16:40:29 -0800

    dwp fixees and action sheet

--- src/pages/tv/tv.component.js
+++ src/pages/tv/tv.component.js
@@ -13,7 +13,8 @@ function Tv() {
         [GALLERY_PATH + "DWP_01.png", GALLERY_PATH + "DWP_02.png"],
         [GALLERY_PATH + "DWP_03.png", GALLERY_PATH + "DWP_04.png"],
         [GALLERY_PATH + "DWP_05.png", GALLERY_PATH + "DWP_06.png"],
-        [GALLERY_PATH + "DWP_07.png", GALLERY_PATH + "DWP_08.png"]
+        [GALLERY_PATH + "DWP_07.png", GALLERY_PATH + "DWP_08.png"],
+        [GALLERY_PATH + "DWP_09.png"]
       ]} />
       <br />
       <br />
